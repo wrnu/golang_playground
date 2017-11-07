79cde45ceea3336aa7848e732ed2fe694057441a
Warren Uniewski <warrenu@appnovation.com>
2017-11-06 21:07:22 -0800

    get test coverage to 100%

--- linklist/linklist.go
+++ linklist/linklist.go
@@ -1,49 +1,45 @@
 package linklist
 
+// Link ... Singly Linked List Type
 type Link struct {
-  value int
-  next  *Link
+	value int
+	next  *Link
 }
 
 func (l *Link) length() int {
-  if l == nil {
-    return 0
-  }
-  n := 0
-  curr := l
-  for curr != nil {
-    curr = curr.next
-    n++
-  }
-  return n
+
+	n := 0
+	curr := l
+	for curr != nil {
+		curr = curr.next
+		n++
+	}
+	return n
 }
 
 func (l *Link) append(value int) {
-  curr := l
 
-  if curr == nil {
-    return
-  }
+	curr := l
 
-  for curr.next != nil {
-    curr = curr.next
-  }
+	for curr.next != nil {
+		curr = curr.next
+	}
 
-  tail := Link{value, nil}
-  curr.next = &tail
+	tail := Link{value, nil}
+	curr.next = &tail
 }
 
 func (l Link) reverse() *Link {
-  curr := &l
-  var next, prev *Link
-  next = nil
-  prev = nil
 
-  for curr != nil {
-    next = curr.next
-    curr.next = prev
-    prev = curr
-    curr = next
-  }
-  return prev
+	curr := &l
+	var next, prev *Link
+	prev = nil
+
+	for curr != nil {
+		next = curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
 }
--- linklist/linklist_test.go
+++ linklist/linklist_test.go
@@ -3,48 +3,70 @@ package linklist
 import "testing"
 
 // test the Link reverse() interface using list of size n
-func test_reverse(n int) bool {
+func testReverse(n int) bool {
 
-  list1 := Link{0, nil}
-  for i := 1; i <= n-1; i++ {
-    list1.append(i)
-  }
+	list1 := Link{0, nil}
+	for i := 1; i <= n-1; i++ {
+		list1.append(i)
+	}
 
-  list2 := Link{n - 1, nil}
-  for i := n - 2; i >= 0; i-- {
-    list2.append(i)
-  }
+	list2 := Link{n - 1, nil}
+	for i := n - 2; i >= 0; i-- {
+		list2.append(i)
+	}
 
-  curr1 := &list1
-  curr2 := list2.reverse()
+	curr1 := &list1
+	curr2 := list2.reverse()
 
-  for curr1 != nil && curr2 != nil {
-    if curr1.value != curr2.value {
-      return false
-    }
-    curr1 = curr1.next
-    curr2 = curr2.next
-  }
-  return true
+	for curr1 != nil && curr2 != nil {
+		if curr1.value != curr2.value {
+			return false
+		}
+		curr1 = curr1.next
+		curr2 = curr2.next
+	}
+	return true
 }
 
-// func TestReverse(t *testing.T) {
-//
-//   cases := []int {1,2,10}
-//   for _, n := range cases {
-//     if !test_reverse(n) {
-//       t.Errorf("Fail %d", n)
-//     }
-//   }
-// }
+func testLength(n int) bool {
+
+	list := Link{0, nil}
+	for i := 1; i <= n-1; i++ {
+		list.append(i)
+	}
+	if list.length() == n {
+		return true
+	}
+	return false
+}
+
+func TestLength(t *testing.T) {
+
+	cases := []int{1, 2, 10}
+	for _, n := range cases {
+		if !testLength(n) {
+			t.Errorf("Fail %d", n)
+		}
+	}
+}
+
+func TestReverse(t *testing.T) {
+
+	cases := []int{1, 2, 10}
+	for _, n := range cases {
+		if !testReverse(n) {
+			t.Errorf("Fail %d", n)
+		}
+	}
+}
 
 func TestAppend(t *testing.T) {
 
-  l := Link{0,nil}
-  l.append(1)
-  if l.length() != 2 {
-    t.Errorf("Length Incorrect")
-  } else if l.next.value != 1 {
-    t.Errorf("Value incorret")
-  }
+	l := Link{0, nil}
+	l.append(1)
+	if l.length() != 2 {
+		t.Errorf("Length Incorrect")
+	} else if l.next.value != 1 {
+		t.Errorf("Value incorret")
+	}
 }
