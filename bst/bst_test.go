2a18a920819b5a94c574efa2b67c2211f1adc691
Warren Uniewski <warrenu@appnovation.com>
2019-02-06 22:06:02 -0800

    fix formmating

--- bst/bst_test.go
+++ bst/bst_test.go
@@ -1,126 +1,126 @@
 package bst
 
 import (
-	"math"
-	"testing"
+  "math"
+  "testing"
 )
 
 func TestFindMin(t *testing.T) {
-	bst := TreeNode{1, 1, nil, nil}
-	bst.insert(0, 0)
-	bst.insert(2, 2)
-	if bst.findMin() != bst.left {
-		t.Errorf("Wrong Min")
-	}
+  bst := TreeNode{1, 1, nil, nil}
+  bst.insert(0, 0)
+  bst.insert(2, 2)
+  if bst.findMin() != bst.left {
+    t.Errorf("Wrong Min")
+  }
 }
 
 func TestFindMax(t *testing.T) {
-	bst := TreeNode{1, 1, nil, nil}
-	bst.insert(0, 0)
-	bst.insert(2, 2)
-	if bst.findMax() != bst.right {
-		t.Errorf("Wrong Max")
-	}
+  bst := TreeNode{1, 1, nil, nil}
+  bst.insert(0, 0)
+  bst.insert(2, 2)
+  if bst.findMax() != bst.right {
+    t.Errorf("Wrong Max")
+  }
 }
 
 func TestDelete(t *testing.T) {
-	left := &TreeNode{0, 0, nil, nil}
-	right := &TreeNode{2, 2, nil, nil}
-	root := &TreeNode{1, 1, left, right}
+  left := &TreeNode{0, 0, nil, nil}
+  right := &TreeNode{2, 2, nil, nil}
+  root := &TreeNode{1, 1, left, right}
 
-	root = root.delete(0)
-	if root.left != nil && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Left Delete Error")
-	}
-	left = &TreeNode{0, 0, nil, nil}
-	right = &TreeNode{2, 2, nil, nil}
-	root = &TreeNode{1, 1, left, right}
+  root = root.delete(0)
+  if root.left != nil && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Left Delete Error")
+  }
+  left = &TreeNode{0, 0, nil, nil}
+  right = &TreeNode{2, 2, nil, nil}
+  root = &TreeNode{1, 1, left, right}
 
-	root = root.delete(2)
-	if root.right != nil && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Right Delete Error")
-	}
+  root = root.delete(2)
+  if root.right != nil && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Right Delete Error")
+  }
 
-	left = &TreeNode{0, 0, nil, nil}
-	right = &TreeNode{2, 2, nil, nil}
-	root = &TreeNode{1, 1, left, right}
+  left = &TreeNode{0, 0, nil, nil}
+  right = &TreeNode{2, 2, nil, nil}
+  root = &TreeNode{1, 1, left, right}
 
-	root = root.delete(1)
-	if root.key != 2 && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Root Delete Error with left and right leaves")
-	}
+  root = root.delete(1)
+  if root.key != 2 && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Root Delete Error with left and right leaves")
+  }
 
-	left = &TreeNode{0, 0, nil, nil}
-	root = &TreeNode{1, 1, left, nil}
+  left = &TreeNode{0, 0, nil, nil}
+  root = &TreeNode{1, 1, left, nil}
 
-	root = root.delete(1)
-	if root.key != 0 && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Root Delete Error with left leaf only")
-	}
+  root = root.delete(1)
+  if root.key != 0 && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Root Delete Error with left leaf only")
+  }
 
-	right = &TreeNode{2, 2, nil, nil}
-	root = &TreeNode{1, 1, nil, right}
+  right = &TreeNode{2, 2, nil, nil}
+  root = &TreeNode{1, 1, nil, right}
 
-	root = root.delete(1)
-	if root.key != 2 && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Root Delete Error with left leaf only")
-	}
+  root = root.delete(1)
+  if root.key != 2 && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Root Delete Error with left leaf only")
+  }
 
-	root = &TreeNode{1, 1, nil, nil}
-	root.insert(10, 10)
-	root.insert(4, 4)
-	root.insert(11, 11)
-	root.delete(10)
-	if root.search(10) != nil && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Delete two child right leaf")
-	}
+  root = &TreeNode{1, 1, nil, nil}
+  root.insert(10, 10)
+  root.insert(4, 4)
+  root.insert(11, 11)
+  root.delete(10)
+  if root.search(10) != nil && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Delete two child right leaf")
+  }
 
-	root = &TreeNode{1, 1, nil, nil}
-	root.insert(0, 0)
-	root.insert(-10, -10)
-	root.insert(-4, -4)
-	root.insert(-11, -11)
-	root.delete(-10)
-	if root.search(-10) != nil && root.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Delete two child left leaf")
-	}
+  root = &TreeNode{1, 1, nil, nil}
+  root.insert(0, 0)
+  root.insert(-10, -10)
+  root.insert(-4, -4)
+  root.insert(-11, -11)
+  root.delete(-10)
+  if root.search(-10) != nil && root.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Delete two child left leaf")
+  }
 
 }
 
 func TestInsert(t *testing.T) {
-	bst := TreeNode{1, 1, nil, nil}
-	bst.insert(0, 0)
-	bst.insert(2, 2)
-	if bst.left.key != 0 || bst.left.value != 0 || !bst.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Left Add Error")
-	}
-	if bst.right.key != 2 || bst.right.value != 2 || !bst.isBST(math.MinInt64, math.MaxInt64) {
-		t.Errorf("Right Add Error")
-	}
+  bst := TreeNode{1, 1, nil, nil}
+  bst.insert(0, 0)
+  bst.insert(2, 2)
+  if bst.left.key != 0 || bst.left.value != 0 || !bst.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Left Add Error")
+  }
+  if bst.right.key != 2 || bst.right.value != 2 || !bst.isBST(math.MinInt64, math.MaxInt64) {
+    t.Errorf("Right Add Error")
+  }
 }
 
 func TestSearch(t *testing.T) {
-	left := &TreeNode{0, 0, nil, nil}
-	right := &TreeNode{2, 2, nil, nil}
-	root := &TreeNode{1, 1, left, right}
-	if root.search(0) != left {
-		t.Errorf("Incorrect Left Node")
-	}
-	if root.search(2) != right {
-		t.Errorf("Incorrect Right Node")
-	}
+  left := &TreeNode{0, 0, nil, nil}
+  right := &TreeNode{2, 2, nil, nil}
+  root := &TreeNode{1, 1, left, right}
+  if root.search(0) != left {
+    t.Errorf("Incorrect Left Node")
+  }
+  if root.search(2) != right {
+    t.Errorf("Incorrect Right Node")
+  }
 }
 
 func TestIsBST(t *testing.T) {
-	left := &TreeNode{0, 0, nil, nil}
-	right := &TreeNode{2, 2, nil, nil}
-	root := &TreeNode{1, 1, left, right}
-	if root.isBST(math.MinInt64, math.MaxInt64) == false {
-		t.Errorf("This is a BST")
-	}
-	root.right = left
-	root.left = right
-	if root.isBST(math.MinInt64, math.MaxInt64) == true {
-		t.Errorf("This is not a BST")
-	}
+  left := &TreeNode{0, 0, nil, nil}
+  right := &TreeNode{2, 2, nil, nil}
+  root := &TreeNode{1, 1, left, right}
+  if root.isBST(math.MinInt64, math.MaxInt64) == false {
+    t.Errorf("This is a BST")
+  }
+  root.right = left
+  root.left = right
+  if root.isBST(math.MinInt64, math.MaxInt64) == true {
+    t.Errorf("This is not a BST")
+  }
 }
--- slinklist/slinklist_test.go
+++ slinklist/slinklist_test.go
@@ -5,192 +5,192 @@ import "testing"
 // test the SLinkList reverse() interface using list of size n
 func testReverse(n int) bool {
 
-	list1 := SLinkList{0, nil}
-	for i := 1; i <= n-1; i++ {
-		list1.append(i)
-	}
+  list1 := SLinkList{0, nil}
+  for i := 1; i <= n-1; i++ {
+    list1.append(i)
+  }
 
-	list2 := SLinkList{n - 1, nil}
-	for i := n - 2; i >= 0; i-- {
-		list2.append(i)
-	}
+  list2 := SLinkList{n - 1, nil}
+  for i := n - 2; i >= 0; i-- {
+    list2.append(i)
+  }
 
-	curr1 := &list1
-	curr2 := list2.reverse()
+  curr1 := &list1
+  curr2 := list2.reverse()
 
-	for curr1 != nil && curr2 != nil {
-		if curr1.value != curr2.value {
-			return false
-		}
-		curr1 = curr1.next
-		curr2 = curr2.next
-	}
-	return true
+  for curr1 != nil && curr2 != nil {
+    if curr1.value != curr2.value {
+      return false
+    }
+    curr1 = curr1.next
+    curr2 = curr2.next
+  }
+  return true
 }
 
 // test the SLinkList reverse() interface using list of size n
 func testReverseRecursion(n int) bool {
 
-	list1 := SLinkList{0, nil}
-	for i := 1; i <= n-1; i++ {
-		list1.append(i)
-	}
+  list1 := SLinkList{0, nil}
+  for i := 1; i <= n-1; i++ {
+    list1.append(i)
+  }
 
-	list2 := SLinkList{n - 1, nil}
-	for i := n - 2; i >= 0; i-- {
-		list2.append(i)
-	}
+  list2 := SLinkList{n - 1, nil}
+  for i := n - 2; i >= 0; i-- {
+    list2.append(i)
+  }
 
-	curr1 := &list1
-	curr2 := (&list2).reverseRecursion()
+  curr1 := &list1
+  curr2 := (&list2).reverseRecursion()
 
-	for curr1 != nil && curr2 != nil {
-		if curr1.value != curr2.value {
-			return false
-		}
-		curr1 = curr1.next
-		curr2 = curr2.next
-	}
-	return true
+  for curr1 != nil && curr2 != nil {
+    if curr1.value != curr2.value {
+      return false
+    }
+    curr1 = curr1.next
+    curr2 = curr2.next
+  }
+  return true
 }
 
 func testLength(n int) bool {
 
-	list := SLinkList{0, nil}
-	for i := 1; i <= n-1; i++ {
-		list.append(i)
-	}
-	if list.length() == n {
-		return true
-	}
-	return false
+  list := SLinkList{0, nil}
+  for i := 1; i <= n-1; i++ {
+    list.append(i)
+  }
+  if list.length() == n {
+    return true
+  }
+  return false
 }
 
 func TestLength(t *testing.T) {
 
-	cases := []int{1, 2, 10}
-	for _, n := range cases {
-		if !testLength(n) {
-			t.Errorf("Fail %d", n)
-		}
-	}
+  cases := []int{1, 2, 10}
+  for _, n := range cases {
+    if !testLength(n) {
+      t.Errorf("Fail %d", n)
+    }
+  }
 }
 
 func TestReverse(t *testing.T) {
 
-	cases := []int{1, 2, 10}
-	for _, n := range cases {
-		if !testReverse(n) {
-			t.Errorf("Fail %d", n)
-		}
-	}
+  cases := []int{1, 2, 10}
+  for _, n := range cases {
+    if !testReverse(n) {
+      t.Errorf("Fail %d", n)
+    }
+  }
 }
 
 func TestReverseRecursion(t *testing.T) {
 
-	cases := []int{1, 2, 10}
-	for _, n := range cases {
-		if !testReverseRecursion(n) {
-			t.Errorf("Fail %d", n)
-		}
-	}
+  cases := []int{1, 2, 10}
+  for _, n := range cases {
+    if !testReverseRecursion(n) {
+      t.Errorf("Fail %d", n)
+    }
+  }
 }
 
 func TestPrepend(t *testing.T) {
 
-	l := SLinkList{0, nil}
-	head := l.prepend(1)
-	if head.length() != 2 {
-		t.Errorf("Length incorrect")
-	}
-	if head.value != 1 {
-		t.Errorf("Head value incorret")
-	}
-	if head.next.value != 0 {
-		t.Errorf("Tail value incorret")
-	}
+  l := SLinkList{0, nil}
+  head := l.prepend(1)
+  if head.length() != 2 {
+    t.Errorf("Length incorrect")
+  }
+  if head.value != 1 {
+    t.Errorf("Head value incorret")
+  }
+  if head.next.value != 0 {
+    t.Errorf("Tail value incorret")
+  }
 }
 
 func TestAppend(t *testing.T) {
 
-	l := SLinkList{0, nil}
-	l.append(1)
-	if l.length() != 2 {
-		t.Errorf("Length Incorrect")
-	}
-	if l.value != 0 {
-		t.Errorf("Head value incorret")
-	}
-	if l.next.value != 1 {
-		t.Errorf("Tail value incorret")
-	}
+  l := SLinkList{0, nil}
+  l.append(1)
+  if l.length() != 2 {
+    t.Errorf("Length Incorrect")
+  }
+  if l.value != 0 {
+    t.Errorf("Head value incorret")
+  }
+  if l.next.value != 1 {
+    t.Errorf("Tail value incorret")
+  }
 }
 
 func TestInsertAfter(t *testing.T) {
 
-	l := SLinkList{0, nil}
-	l.insertAfter(1)
-	if l.length() != 2 {
-		t.Errorf("Length Incorrect")
-	}
-	if l.value != 0 {
-		t.Errorf("Head value incorret")
-	}
-	if l.next.value != 1 {
-		t.Errorf("Tail value incorret")
-	}
+  l := SLinkList{0, nil}
+  l.insertAfter(1)
+  if l.length() != 2 {
+    t.Errorf("Length Incorrect")
+  }
+  if l.value != 0 {
+    t.Errorf("Head value incorret")
+  }
+  if l.next.value != 1 {
+    t.Errorf("Tail value incorret")
+  }
 }
 
 func TestDelete(t *testing.T) {
 
-	l := &SLinkList{0, nil}
-	l.append(-10)
-	l.append(10)
-	l.append(100)
+  l := &SLinkList{0, nil}
+  l.append(-10)
+  l.append(10)
+  l.append(100)
 
-	l = l.delete(-10)
-	if l.search(-10) != nil && l.length() == 3 {
-		t.Errorf("Failed to delete middle element")
-	}
+  l = l.delete(-10)
+  if l.search(-10) != nil && l.length() == 3 {
+    t.Errorf("Failed to delete middle element")
+  }
 
-	l = l.delete(100)
-	if l.search(1000) != nil && l.length() == 2 {
-		t.Errorf("Failed to delete tail element")
-	}
+  l = l.delete(100)
+  if l.search(1000) != nil && l.length() == 2 {
+    t.Errorf("Failed to delete tail element")
+  }
 
-	l = l.delete(0)
-	if l.search(0) != nil && l.length() == 1 {
-		t.Errorf("Failed to delete head element")
-	}
+  l = l.delete(0)
+  if l.search(0) != nil && l.length() == 1 {
+    t.Errorf("Failed to delete head element")
+  }
 
-	len := l.length()
-	l = l.delete(101)
-	if l.length() != len {
-		t.Errorf("Failed delete of value not in list")
-	}
+  len := l.length()
+  l = l.delete(101)
+  if l.length() != len {
+    t.Errorf("Failed delete of value not in list")
+  }
 
-	l = l.delete(10)
-	if l != nil {
-		t.Errorf("Failed to delete last element")
-	}
+  l = l.delete(10)
+  if l != nil {
+    t.Errorf("Failed to delete last element")
+  }
 }
 
 func TestSearch(t *testing.T) {
 
-	l := SLinkList{0, nil}
-	l.append(1)
-	l.append(2)
+  l := SLinkList{0, nil}
+  l.append(1)
+  l.append(2)
 
-	if l.search(0).value != 0 {
-		t.Errorf("Failed to find node")
-	}
-	if l.search(1).value != 1 {
-		t.Errorf("Failed to find node")
-	}
-	if l.search(2).value != 2 {
-		t.Errorf("Failed to find node")
-	}
-	if l.search(3) != nil {
-		t.Errorf("Found node that doesn't exist")
-	}
+  if l.search(0).value != 0 {
+    t.Errorf("Failed to find node")
+  }
+  if l.search(1).value != 1 {
+    t.Errorf("Failed to find node")
+  }
+  if l.search(2).value != 2 {
+    t.Errorf("Failed to find node")
+  }
+  if l.search(3) != nil {
+    t.Errorf("Found node that doesn't exist")
+  }
 }
